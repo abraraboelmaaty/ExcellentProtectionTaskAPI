3a2be1fe8f3d4c170fbcd59c2b2bc43e31467673
abraraboelmaaty <abraraboelmaaty24@gmail.com>
2022-08-12 15:37:16 +0200

    add controllers

---
+++ ExcellentProtectionTaskAPI/ExcellentProtectionTaskAPI/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using ExcellentProtectionTaskAPI.Models;
+using ExcellentProtectionTaskAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExcellentProtectionTaskAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        IRepository<Order> orderRepo;
+        public OrderController(IRepository<Order> _orderRepo)
+        {
+            orderRepo = _orderRepo;
+        }
+        [HttpPost]
+        public ActionResult Add(Order order)
+        {
+            orderRepo.Create(order);
+            return Created("uri", order);
+        }
+        [HttpGet]
+        public ActionResult getAll()
+        {
+            if (orderRepo.GetAll().Count > 0)
+                return Ok(orderRepo.GetAll());
+            else
+                return NotFound();
+        }
+    }
+}
---
+++ ExcellentProtectionTaskAPI/ExcellentProtectionTaskAPI/Controllers/PaymentController.cs
@@ -0,0 +1,32 @@
+﻿using ExcellentProtectionTaskAPI.Models;
+using ExcellentProtectionTaskAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExcellentProtectionTaskAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        IRepository<Payment> paymentRepo;
+        public PaymentController(IRepository<Payment> _paymentRepo)
+        {
+            paymentRepo = _paymentRepo;
+        }
+        [HttpPost]
+        public ActionResult Add(Payment payment)
+        {
+            paymentRepo.Create(payment);
+            return Created("uri", payment);
+        }
+        [HttpGet]
+        public ActionResult getAll()
+        {
+            if (paymentRepo.GetAll().Count > 0)
+                return Ok(paymentRepo.GetAll());
+            else
+                return NotFound();
+        }
+    }
+}
